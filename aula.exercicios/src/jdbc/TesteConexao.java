b768c8c41dfa1b0f73848d9282a0d682be1ddd14
mroya <royamarcio@gmail.com>
2023-04-11 17:17:34 -0300

    update

--- aula.exercicios/src/jdbc/TesteConexao.java
+++ aula.exercicios/src/jdbc/TesteConexao.java
@@ -9,9 +9,9 @@ public class TesteConexao {
 	
 	public static void main(String[] args) throws SQLException {
 		
-		final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
-		final String usuario = "root";
-		final String senha = "royamysql";
+		final String url = "";
+		final String usuario = "";
+		final String senha = "";
 		
 		Connection conexao = DriverManager.getConnection(url, usuario, senha);
 		
